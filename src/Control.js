76a3e4f333a457783cf00f4f357f1694d10d1896
Gal Tashma <galtashma@gmail.com>
2019-10-14 21:54:34 +0300

    Imported work from old admin-netlifycms to here

--- src/Control.js
+++ src/Control.js
@@ -1,5 +1,25 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {Container, GroupListItem, PlusListItem, LinkListItem} from './components.js';
+import { fromJS } from 'immutable';
+
+import AsyncSelect from 'react-select/lib/Async';
+
+console.log(AsyncSelect);
+
+const defaultLinks = [
+	{title: "One",   href: "#one"},
+	{title: "Two",   href: "#two"},
+	{title: "Three", href: "#three"},
+	{title: "Four",  children: [
+		{title: "Four point One", href: "#four.one"}
+	]},
+];
+
+function renderLinkItem(item, index){
+  const key="render-item"+index + Math.random();
+  return <LinkListItem key={key} value={item.get("href")}>{item.get("title")}</LinkListItem>;
+}
 
 export default class Control extends React.Component {
   static propTypes = {
@@ -13,6 +33,72 @@ export default class Control extends React.Component {
     value: '',
   }
 
+  getValue = () => {
+    return this.props.value || fromJS(defaultLinks);
+  }
+
+  renderLinkItems = () => {
+    const value = this.getValue();
+    return value.map((item, index) => {
+      let children = item.get("children");
+      
+      if (children !== undefined && children.size > 0) {
+        const childrenItems = children.map((child, i) => {
+          return renderLinkItem(child, i);
+        });
+
+        return <GroupListItem key={index}>{item.get("title")}
+            <ul>{childrenItems}</ul>
+          </GroupListItem>;
+      }
+      
+      return renderLinkItem(item, index); 
+    });
+  }
+
+  handleAdd = (title) => {
+    const listValue = this.getValue();
+    const { onChange } = this.props;
+    const parsedValue = fromJS({title: title, href: "#new!"});
+    console.log(parsedValue);
+    onChange((listValue || List()).push(parsedValue));
+  }
+
+  handleKeyUp = (e) => {
+    e.preventDefault();
+    if (e.which == 13) { // Enter
+      let content = e.target.textContent;
+      e.target.textContent = "";
+      this.handleAdd(content); 
+    }
+  }
+
+  handleFocus = (e) => {
+    if (e.target.textContent === "New Item") {
+      e.target.textContent = "";
+    }
+  }
+
+  handleUnFocus = (e) => {
+    if (e.target.textContent.length == 0) { 
+      e.target.textContent = "New Item";
+    }
+  }
+
+  loadOptions = (inputValue, callback) => {
+    console.log("load options", inputValue);
+    callback([
+      { value: "AAA", label: "aaa"},
+      { value: "BBB", label: "bbb"},
+      { value: "CCC", label: "ccc"},
+    ]);
+  };
+
+  handleInputChange = (newValue) => {
+    console.log("input change", newValue);
+    return newValue;
+  };
+
   render() {
     const {
       forID,
@@ -21,14 +107,24 @@ export default class Control extends React.Component {
       classNameWrapper,
     } = this.props;
 
+    const linkItems = this.renderLinkItems();
+
     return (
-      <input
-        type="text"
-        id={forID}
-        className={classNameWrapper}
-        value={value || ''}
-        onChange={e => onChange(e.target.value)}
-      />
+      <Container>
+        {linkItems}
+        <PlusListItem 
+          key="plus-list-item"
+          onKeyUp={this.handleKeyUp}
+          onFocus={this.handleFocus} 
+          onBlur={this.handleUnFocus}>
+          <div contenteditable="true">New Item</div>
+        </PlusListItem>
+        <AsyncSelect
+          loadOptions={this.loadOptions}
+          onInputChange={this.handleInputChange}
+        />
+        
+      </Container>
     );
   }
 }

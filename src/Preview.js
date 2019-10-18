fa5ef9361d31d357b3f056fd3bff474aa90613f8
Gal Tashma <galtashma@gmail.com>
2019-10-18 12:22:46 +0300

    Added simple yaml preview

--- src/Preview.js
+++ src/Preview.js
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import YAML from 'js-yaml';
 
 export default function Preview({ value }) {
-  return <div>{ value }</div>;
+  const content = YAML.dump(value.toJS()) ; 
+  return <div><pre>
+    { content }
+  </pre></div>;
 }
 
 Preview.propTypes = {
